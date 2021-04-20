340c93f8215c9cff6cc6159241c082a3d35ab3d1
xzackli <xzackli@gmail.com>
2021-04-20 16:50:35 -0400

    generate nside 256 planck maps and masks

--- downgrade_planck.jl
+++ downgrade_planck.jl
@@ -1,7 +1,6 @@
 using Healpix
 using Healpix: udgrade
 import Healpix: saveToFITS 
-using CFITSIO
 using PyCall
 hp = pyimport("healpy")
 
@@ -23,7 +22,7 @@ end
 ## write maps
 for s in ("1", "2"), f in ("100", "143", "217")
     fname = "HFI_SkyMap_$(f)_2048_R3.01_halfmission-$(s).fits"
-    mapdir = "/tigress/zequnl/planck18/maps/"
+    mapdir = "/tigress/zequnl/planck/maps/"
     cols = [process_map(i, nside_out, mapdir, fname) for i in 1:10]
     hp.fitsfunc.write_map("data/plancklowres/nside$(nside_out)_$(fname)", cols, nest=true, overwrite=true)
 end
@@ -31,11 +30,40 @@ end
 ## write masks
 for s in ("1", "2"), f in ("100", "143", "217"), type in ("temperature", "polarization")
     fname = "COM_Mask_Likelihood-$(type)-$(f)-hm$(s)_2048_R3.00.fits"
-    mapdir = "/tigress/zequnl/planck18/masks/"
+    mapdir = "/tigress/zequnl/planck/masks/"
 
     cols = udgrade(readMapFromFITS(joinpath(mapdir,fname), 1, Float64), nside_out).pixels
-    hp.fitsfunc.write_map("data/plancklowres/nside$(nside_out)_$(fname)", cols, nest=true, overwrite=true)
+    hp.fitsfunc.write_map("data/plancklowres/nside$(nside_out)_$(fname)", cols, nest=false, overwrite=true)
 end
+
+
+##
+function test_read_map(fname, col=1)
+    m = readMapFromFITS(fname, col, Float64)
+end
+
+
+println("testing...")
+for s in ("1", "2"), f in ("100", "143", "217")
+    fname = "HFI_SkyMap_$(f)_2048_R3.01_halfmission-$(s).fits"
+    test_read_map("data/plancklowres/nside$(nside_out)_$(fname)", 1)
+    test_read_map("data/plancklowres/nside$(nside_out)_$(fname)", 2)
+    test_read_map("data/plancklowres/nside$(nside_out)_$(fname)", 3)
+end
+for s in ("1", "2"), f in ("100", "143", "217"), type in ("temperature", "polarization")
+    fname = "COM_Mask_Likelihood-$(type)-$(f)-hm$(s)_2048_R3.00.fits"
+    test_read_map("data/plancklowres/nside$(nside_out)_$(fname)")
+end
+
+
+##
+cd("data")
+run(`tar -zcvf plancklowres.tar.gz plancklowres`)
+filename = "plancklowres.tar.gz"
+##
+using Tar, Inflate, SHA
+println("sha256: ", bytes2hex(open(sha256, filename)))
+println("git-tree-sha1: ", Tar.tree_hash(IOBuffer(inflate_gzip(filename))))
 
 
 ##
